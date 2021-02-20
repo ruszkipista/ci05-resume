3f950681dda71a47e90cad07f45fe80716fe043b
ruszkipista <oi5672@gmail.com>
2021-02-20 14:15:57 +0000

    fix user display at start on github page

--- assets/js/github.js
+++ assets/js/github.js
@@ -1,3 +1,5 @@
+window.addEventListener("load",fetchGitHubInformation);
+
 function displayUser(user) {
     const userDataElement = document.getElementById('github-user-data');
     userDataElement.innerHTML = `
@@ -7,32 +9,25 @@ function displayUser(user) {
             </span>
         </h2>
         <div class="github-content">
-            <div class="github-avatar">
-                <a href="${user.html_url}" target="_blank">
-                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
-                </a>
-            </div>
+            <a href="${user.html_url}" target="_blank">
+                <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+            </a>
             <p>Followers: ${user.followers} - Following: ${user.following} - Repos: ${user.public_repos}</p>
         </div>`;
 }
 function displayRepos(repos) {
-    if (repos.length == 0) {
-        return `<div class="clearfix repo-list">No repos!</div>`;
-    }
-
-    var listItemsHTML = repos.map(function (repo) {
-        return `<li>
-                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-                </li>`;
-    });
-
     const repoDataElement = document.getElementById('github-repo-data');
-    repoDataElement.innerHTML = `
-    <div class="repo-list">
-        <p><strong>Repo List:</strong></p>
-        <ul>${listItemsHTML.join("\n")}</ul>
-    </div>
-    `;
+    if (repos.length == 0) {
+        repoDataElement.innerHTML = `<div class="repo-list">No repos!</div>`;
+    } else {
+        var listItemsHTML = repos.map(function (repo) {
+            return `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`;
+        });
+        repoDataElement.innerHTML = `<div class="repo-list">
+                                        <p><strong>Repo List:</strong></p>
+                                         <ul>${listItemsHTML.join('')}</ul>
+                                     </div>`;
+    }
 }
 function status(response) {
     if (response.status >= 200 && response.status < 300) {
@@ -42,21 +37,17 @@ function status(response) {
     }
 }
 
-function json(response) {
-    return
-}
-
 function fetchGitHubInformation() {
     const usernameElement = document.getElementById('github-user-name');
     const userdataElement = document.getElementById('github-user-data');
     const userrepoElement = document.getElementById('github-repo-data');
+    userrepoElement.innerHTML = '';
     if (!usernameElement.value) {
         userdataElement.innerHTML = '<h4>Please enter a GitHub user name</h4>';
         return;
     }
 
-    userdataElement.innerHTML = '<div id="loader"><img src="./assets/css/octocat-spinner-smil.min.svg" alt="loading..."></div>';
-    userrepoElement.innerHTML = '';
+    userdataElement.innerHTML = '<img id="loader" src="./assets/css/octocat-spinner-smil.min.svg" alt="loading...">';
     // https://www.pluralsight.com/guides/using-fetch-with-github-api-v3
     // https://developers.google.com/web/updates/2015/03/introduction-to-fetch
     fetch(`https://api.github.com/users/${usernameElement.value}`, { headers: { 'Accept': 'application/vnd.github.v3+json' } })
